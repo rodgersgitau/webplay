7d6e63fff61007465e5a037362eaf5495e096e70
Rodgers Gitau <rodgers.gitau@gmail.com>
2025-09-15 00:15:26 +0300

    chore: project reset

--- apps/web/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { nextJsConfig } from "@webplay/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
--- packages/ui/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { WebplayCanvas } from "./components/canvas.js";
-export { WebplayButton } from "./components/button.js";
-export { WebplayCard } from "./components/card.js";
